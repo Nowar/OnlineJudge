8c232b5a72f490da039cb9947ab62fb075be9398
WenHan Gu <wenhan.gu@gmail.com>
2015-07-25 21:40:52 +0800

    Add code template for each test.

---
+++ template/UVa.h
@@ -0,0 +1,39 @@
+//==========================================================================//
+// Copyright (c) 2010-2015 Wen-Han Gu. All rights reserved.
+//
+// Author: Wen-Han Gu (a.k.a. Nowar)
+// E-mail: wenhan.gu <at> gmail.com
+//
+// Any advice is welcome. Thank you!
+//==========================================================================//
+#ifndef UVA_H
+#define UVA_H
+
+// Include all headers in here since UVa can only submit one merged file.
+// This is not a good behavior but it works.
+
+// C++
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+// C
+#include <cassert>
+#include <cstring>
+
+#ifndef UASSERT
+#ifndef ONLINE_JUDGE
+#define UASSERT(expr) assert (expr)
+#else
+#define UASSERT(expr)
+#endif
+#endif
+
+// Define your data structure here.
+
+#endif // UVA_H
---
+++ template/main.cpp
@@ -0,0 +1,33 @@
+//==========================================================================//
+// Copyright (c) 2010-2015 Wen-Han Gu. All rights reserved.
+//
+// Author: Wen-Han Gu (a.k.a. Nowar)
+// E-mail: wenhan.gu <at> gmail.com
+//
+// Any advice is welcome. Thank you!
+//==========================================================================//
+
+#include "UVa.h"
+
+static int runUVa(std::istream &is, std::ostream &os) {
+  // Implement here.
+}
+
+int main(int argc, char **argv) {
+#ifndef ONLINE_JUDGE
+  assert (argc > 1 && "We expect input from argv[1] for debugging purpose.");
+  std::ifstream ifs;
+  ifs.open(argv[1]);
+  assert (ifs.good() && "Test data cannot open.");
+  std::istream &is = ifs;
+#else
+  std::istream &is = std::cin;
+#endif
+
+  int ret = runUVa(is, std::cout);
+
+#ifndef ONLINE_JUDGE
+  ifs.close();
+#endif
+  return ret;
+}
