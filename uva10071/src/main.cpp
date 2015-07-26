54b702ef6e4ed5ec00d4b40eae807b16bd5a9b16
WenHan Gu <wenhan.gu@gmail.com>
2015-07-27 00:31:48 +0800

    Finish UVa10071: Back to High School Physics.
    Time: 0.020
    Best: 0.000
    Rank: 5218

---
+++ uva10071/src/main.cpp
@@ -0,0 +1,38 @@
+//==========================================================================//
+// Copyright (c) 2010-2015 Wen-Han Gu. All rights reserved.
+//
+// Author: Wen-Han Gu (a.k.a. Nowar)
+// E-mail: wenhan.gu <at> gmail.com
+//
+// Any advice is welcome. Thank you!
+//==========================================================================//
+
+#include "UVa.h"
+
+static int runUVa(std::istream &is, std::ostream &os) noexcept {
+  int a, b;
+  while (scanf("%d %d", &a, &b) != EOF) {
+    printf("%d\n", 2*a*b);
+  }
+  // Implement here.
+  return 0;
+}
+
+int main(int argc, char **argv) noexcept {
+#ifndef ONLINE_JUDGE
+  assert (argc > 1 && "We expect input from argv[1] for debugging purpose.");
+  std::ifstream ifs;
+  ifs.open(argv[1]);
+  assert (ifs.good() && "Test data cannot open.");
+  std::istream &is = ifs;
+#else
+  std::istream &is = std::cin;
+#endif
+
+  int ret = runUVa(is, std::cout);
+
+#ifndef ONLINE_JUDGE
+  ifs.close();
+#endif
+  return ret;
+}
