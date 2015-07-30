e1fdd7f080cd7332b9466c2ce2eb4a7c0c19c8ae
WenHan Gu <wenhan.gu@gmail.com>
2015-07-30 21:03:43 +0800

    Finish UVa11799 - Horror Dash.
    Time: 0.001
    Best: 0.000
    Rank: 246

---
+++ UVa/11799/src/main.cpp
@@ -0,0 +1,48 @@
+//==========================================================================//
+// Copyright (c) 2010-2015 Wen-Han Gu. All rights reserved.
+//
+// Author: Wen-Han Gu (a.k.a. Nowar)
+// E-mail: wenhan.gu <at> gmail.com
+//
+// Any advice is welcome. Thank you!
+//==========================================================================//
+
+#include "UVa.h"
+
+static int runUVa(std::istream &is, std::ostream &os) noexcept {
+  // Implement here.
+  int Count;
+  is >> Count;
+  std::string Input;
+  std::getline(is, Input);
+  REP (i, 0, Count) {
+    int N;
+    is >> N;
+    int Num, Max = 1;
+    while (N-- > 0) {
+      is >> Num;
+      if (Num > Max)  Max = Num;
+    }
+    os << "Case " << i+1 << ": " << Max << "\n";
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) noexcept {
+#ifndef ONLINE_JUDGE
+  assert (argc > 1 && "We expect input from argv[1] for debugging purpose.");
+  std::ifstream ifs;
+  ifs.open(argv[1]);
+  assert (ifs.good() && "Test data cannot open.");
+  std::istream &is = ifs;
+#else
+  std::istream &is = std::cin;
+#endif
+
+  int ret = runUVa(is, std::cout);
+
+#ifndef ONLINE_JUDGE
+  ifs.close();
+#endif
+  return ret;
+}
