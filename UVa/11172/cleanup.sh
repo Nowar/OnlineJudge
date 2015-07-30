f7a6d8f86605b3443de3ae1eb5405437e95b6d2b
WenHan Gu <wenhan.gu@gmail.com>
2015-07-30 20:07:51 +0800

    Finish UVa11172 - Relational Operator.
    Time: 0.000
    Best: 0.000
    Rank: 2342

---
+++ UVa/11172/cleanup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf a.out* output error
+rm -f merged.cpp
---
+++ UVa/11172/src/main.cpp
@@ -0,0 +1,43 @@
+//==========================================================================//
+// Copyright (c) 2010-2015 Wen-Han Gu. All rights reserved.
+//
+// Author: Wen-Han Gu (a.k.a. Nowar)
+// E-mail: wenhan.gu <at> gmail.com
+//
+// Any advice is welcome. Thank you!
+//==========================================================================//
+
+#include "UVa.h"
+
+static int runUVa(std::istream &is, std::ostream &os) noexcept {
+  // Implement here.
+  int Count;
+  int64_t a, b;
+  is >> Count;
+  REP(i, 0, Count) {
+    is >> a >> b;
+    if (a > b)  os << ">\n";
+    else if (a < b) os << "<\n";
+    else  os << "=\n";
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) noexcept {
+#ifndef ONLINE_JUDGE
+  assert (argc > 1 && "We expect input from argv[1] for debugging purpose.");
+  std::ifstream ifs;
+  ifs.open(argv[1]);
+  assert (ifs.good() && "Test data cannot open.");
+  std::istream &is = ifs;
+#else
+  std::istream &is = std::cin;
+#endif
+
+  int ret = runUVa(is, std::cout);
+
+#ifndef ONLINE_JUDGE
+  ifs.close();
+#endif
+  return ret;
+}
