fe7c9dcc7d8f294c6c2ecb85cb9f9f9513ab0d7f
WenHan Gu <wenhan.gu@gmail.com>
2015-07-30 20:28:21 +0800

    WIP UVa11727.

---
+++ UVa/11727/src/main.cpp
@@ -0,0 +1,42 @@
+//==========================================================================//
+// Copyright (c) 2010-2015 Wen-Han Gu. All rights reserved.
+//
+// Author: Wen-Han Gu (a.k.a. Nowar)
+// E-mail: wenhan.gu <at> gmail.com
+//
+// Any advice is welcome. Thank you!
+//==========================================================================//
+
+#include "UVa.h"
+
+static int runUVa(std::istream &is, std::ostream &os) noexcept {
+  // Implement here.
+  int Count;
+  is >> Count;
+  REP (i, 0, Count) {
+    int A[3];
+    is >> A[0] >> A[1] >> A[2];
+    std::sort(A, A+3);
+    os << "Case " << i+1 << ": " << A[1] << "\n";
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) noexcept {
+#ifndef ONLINE_JUDGE
+  assert (argc > 1 && "We expect input from argv[1] for debugging purpose.");
+  std::ifstream ifs;
+  ifs.open(argv[1]);
+  assert (ifs.good() && "Test data cannot open.");
+  std::istream &is = ifs;
+#else
+  std::istream &is = std::cin;
+#endif
+
+  int ret = runUVa(is, std::cout);
+
+#ifndef ONLINE_JUDGE
+  ifs.close();
+#endif
+  return ret;
+}
