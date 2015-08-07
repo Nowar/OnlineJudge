9e818d76d26eb5ec3206c6a7be0a9ce77dec5d8e
WenHan Gu <wenhan.gu@gmail.com>
2015-08-08 01:37:46 +0800

    Finish UVa146 - ID Codes.
    Time: 0.001
    Best: 0.000
    Rank: 2724 / 9441

---
+++ UVa/00146/src/main.cpp
@@ -0,0 +1,41 @@
+//==========================================================================//
+// Copyright (c) 2010-2015 Wen-Han Gu. All rights reserved.
+//
+// Author: Wen-Han Gu (a.k.a. Nowar)
+// E-mail: wenhan.gu <at> gmail.com
+//
+// Any advice is welcome. Thank you!
+//==========================================================================//
+
+#include "UVa.h"
+
+static int runUVa(std::istream &is, std::ostream &os) noexcept {
+  // Implement here.
+  std::string Input;
+  while (is >> Input, Input != "#") {
+    if (std::next_permutation(ALL(Input)))
+      os << Input << "\n";
+    else
+      os << "No Successor\n";
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) noexcept {
+#ifndef ONLINE_JUDGE
+  assert (argc > 1 && "We expect input from argv[1] for debugging purpose.");
+  std::ifstream ifs;
+  ifs.open(argv[1]);
+  assert (ifs.good() && "Test data cannot open.");
+  std::istream &is = ifs;
+#else
+  std::istream &is = std::cin;
+#endif
+
+  int ret = runUVa(is, std::cout);
+
+#ifndef ONLINE_JUDGE
+  ifs.close();
+#endif
+  return ret;
+}
