1672f897e06d7fe3299588bee2daa303e651be3f
WenHan Gu <wenhan.gu@gmail.com>
2015-08-16 09:37:45 +0800

    Finish UVa12405 - Scarecrow.
    Time: 0.000
    Best: 0.000
    Rank: 81 / 1675

---
+++ UVa/12405/src/main.cpp
@@ -0,0 +1,52 @@
+//==========================================================================//
+// Copyright (c) 2010-2015 Wen-Han Gu. All rights reserved.
+//
+// Author: Wen-Han Gu (a.k.a. Nowar)
+// E-mail: wenhan.gu <at> gmail.com
+//
+// Any advice is welcome. Thank you!
+//==========================================================================//
+
+#include "UVa.h"
+
+static int runUVa(std::istream &is, std::ostream &os) noexcept {
+  // Implement here.
+  int T;
+  is >> T;
+  char Input[100];
+  REP (i, 0, T) {
+    int N;
+    is >> N;
+    REP (j, 0, N)
+      is >> Input[j];
+
+    int Max = 0;
+    REP (j, 0, N) {
+      if (Input[j] == '.') {
+        Max++;
+        j += 3-1;
+      }
+    }
+    os << "Case " << i+1 << ": " << Max << "\n";
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) noexcept {
+#ifndef ONLINE_JUDGE
+  assert (argc > 1 && "We expect input from argv[1] for debugging purpose.");
+  std::ifstream ifs;
+  ifs.open(argv[1]);
+  assert (ifs.good() && "Test data cannot open.");
+  std::istream &is = ifs;
+#else
+  std::istream &is = std::cin;
+#endif
+
+  int ret = runUVa(is, std::cout);
+
+#ifndef ONLINE_JUDGE
+  ifs.close();
+#endif
+  return ret;
+}
